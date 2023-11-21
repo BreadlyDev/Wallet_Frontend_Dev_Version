79674d5a59b68d1c4b033cae31b5bdf7d4e8f6eb
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-22 00:10:16 +0600

    merge fixed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,15 @@
 import React, {ReactNode} from 'react';
 import Nav from "../Nav/Nav.tsx";
-<<<<<<< HEAD
+
 import styles from "./Layout.module.scss"
-=======
-import classes from './Layout.module.scss'
->>>>>>> 041d84b4b4b36261bf5f35c4e55c4766769ea36e
+
 type LayoutProps = {
     children: ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
     return (
-<<<<<<< HEAD
         <div className={styles.Layout}>
-=======
-        <div className={classes.Layout}>
->>>>>>> 041d84b4b4b36261bf5f35c4e55c4766769ea36e
             <header>
                 <Nav/>
             </header>
@@ -25,5 +19,4 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
         </div>
     )
 };
-
 export default Layout;
