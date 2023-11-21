a9bc01a395d814ad256d4b90406b6376cf20fd1e
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-21 23:08:38 +0600

    fixed bags

--- src/components/Nav/NavItem/NavItem.tsx
+++ src/components/Nav/NavItem/NavItem.tsx
@@ -1,7 +1,5 @@
-// NavItem.tsx
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './NavItem.module.scss';
 
 interface NavItemProps {
@@ -16,7 +14,7 @@ const NavItem: React.FC<NavItemProps> = ({ to, children, isActive, onClick, icon
     return (
         <li className={isActive ? `${styles.NavItem} ${styles.active}` : styles.NavItem} onClick={onClick}>
             <img src={icon} alt="icons fur navlinks" />
-            <Link to={to}>{children}</Link>
+            <NavLink to={to}>{children}</NavLink>
         </li>
     );
 };
