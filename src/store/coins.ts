139eb7daa315f67d048e6d363fcdf28ba738d568
baitemir <baitemir1706@gmail.com>
2023-11-22 20:14:29 +0600

    increment and decrement balance store and services created

---
+++ src/store/coins.ts
@@ -0,0 +1,24 @@
+import { makeAutoObservable } from 'mobx';
+import { decrementBalance, incrementBalance } from '../services/CoinsService';
+
+class BalanceStore {
+  currentBalance = 0
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  incrementBalance(amount:number) {
+    this.currentBalance += amount;
+    incrementBalance(amount)
+  }
+
+  decrementBalance(amount:number) {
+    this.currentBalance -= amount;
+    decrementBalance(amount)
+  }
+}
+
+const balanceStore = new BalanceStore();
+
+export default balanceStore;
