08acfcd5a9bc0e4a97c3075ed4329d79b8f88907
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-23 14:24:51 +0600

    get data in websoccet

--- src/components/CoinChart/CoinChart.tsx
+++ src/components/CoinChart/CoinChart.tsx
@@ -1,20 +1,56 @@
-import React from "react";
+import {useEffect, useState} from 'react';
 
-const CoinChart: React.FC = () => {
-
-
-
-    return <div>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur consequuntur eligendi nam
-            neque, saepe voluptas! Adipisci blanditiis consectetur consequuntur culpa cumque deleniti et explicabo fuga
-            fugit, id impedit labore laudantium libero magni maiores minima minus molestias nobis non nostrum omnis
-            optio porro possimus praesentium quia recusandae repudiandae saepe sint temporibus tenetur ullam unde veniam
-            veritatis! Ab, aut autem, blanditiis dolore ex fuga fugiat hic impedit ipsa ipsam itaque laboriosam magnam
-            magni molestias mollitia nam nemo nisi officia optio ratione recusandae reiciendis reprehenderit rerum sint
-            suscipit vel voluptate! Alias animi autem deleniti deserunt hic illo labore laborum quasi vel voluptatem!
-        </p>
-    </div>
+interface CoinChart {
+    id: string;
+    s: string; // Название
+    c: number; // Цена
 }
 
+const CoinChart = () => {
+    const [dataList, setDataList] = useState<any[]>([]);
+
+    useEffect(() => {
+        const socket = new WebSocket('wss://wallet-dev-server-dev-sqsk.2.ie-1.fl0.io/prices/?coin_name=BTCUSDT');
+        socket.onmessage = (event) => {
+            const receivedData = JSON.parse(event.data);
+            setDataList([receivedData]);
+        };
+        return () => {
+            socket.close();
+        };
+    }, []);
+
+    let trade: any;
+    trade = dataList.map((item, index) => {
+        const {E, c, s} = JSON.parse(item);
+        const newData = {E, c, s};
+        const timestamp = newData.E
+        const date = new Date(timestamp);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const formattedDateTime = ` ${hours}:${minutes}:${seconds}`;
+
+        return (
+            <ul key={index}>
+                <h2>name: {newData.s}</h2>
+                <li>time : {formattedDateTime}</li>
+                <li>price :{newData.c}</li>
+            </ul>
+        );
+    });
+
+
+    return (
+        <div>
+            <h1>WebSocket Data Display</h1>
+            <div style={{color: "white"}}>
+                {trade}
+            </div>
+        </div>
+    );
+};
+
 export default CoinChart;
+
+
