71d9f8f1c891b47a9aa8d4fad5d1fb12b4a43681
AsanbaevBaitemir <baitemir1706@gmail.com>
2023-12-20 23:51:04 +0600

    nav click fix

--- src/components/Nav/NavItem/NavItem.tsx
+++ src/components/Nav/NavItem/NavItem.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
-import styles from './NavItem.module.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./NavItem.module.scss";
 
 interface NavItemProps {
-    to: string;
-    children: React.ReactNode;
-    isActive: boolean;
-    onClick: () => void;
-    icon: string;
+  to: string;
+  children: React.ReactNode;
+  isActive: boolean;
+  onClick: () => void;
+  icon: string;
 }
 
-const NavItem: React.FC<NavItemProps> = ({to, children, isActive, onClick, icon}) => {
-    return (
-        <li className={isActive ? `${styles.NavItem} ${styles.active}` : styles.NavItem} onClick={onClick}>
-
-            <NavLink to={to}>
-                <img src={icon} alt="icons fur navlinks"/>{children}</NavLink>
-        </li>
-    );
+const NavItem: React.FC<NavItemProps> = ({
+  to,
+  children,
+  isActive,
+  onClick,
+  icon,
+}) => {
+  return (
+    <li
+      className={
+        isActive ? `${styles.NavItem} ${styles.active}` : styles.NavItem
+      }
+      onClick={onClick}
+    >
+      <NavLink to={to}>
+        <img src={icon} alt="icons fur navlinks" />
+        {children}
+      </NavLink>
+    </li>
+  );
 };
 
 export default NavItem;
