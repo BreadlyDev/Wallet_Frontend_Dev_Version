37d124cc3aed2918053f625a580f3f435a9c2cc6
AsanbaevBaitemir <baitemir1706@gmail.com>
2023-12-21 19:59:00 +0600

    profile cmoplete

--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import classes from './ProfileSection.module.scss'
+import { Link } from "react-router-dom";
+import classes from "./ProfileSection.module.scss";
 
 type Props = {};
 
@@ -10,10 +10,13 @@ export default function ProfileSection({}: Props) {
       <div>
         <div className={classes.img}></div>
         <div>
-            <span>Keanu Reeves</span>
-            <span>neo@gmail.com</span>
+          <span>Keanu Reeves</span>
+          <span>neo@gmail.com</span>
         </div>
-        <Link to={"profile/edit"}>Edit</Link>
+        <div>
+          <Link to={"profile/edit"}>Edit</Link>
+          <Link to={"profile/edit"}>Logout</Link>
+        </div>
       </div>
     </div>
   );
--- src/components/ProfileWidget/History/History.tsx
+++ src/components/ProfileWidget/History/History.tsx
@@ -1,7 +1,33 @@
-type Props = {}
+import { useState } from "react";
+import classes from './History.module.scss'
+import Assets from "../Assets/Assets";
+type Props = {};
 
 export default function History({}: Props) {
+  const [type, setType] = useState('all');
   return (
-    <div>History</div>
-  )
+    <div className={classes.History}>
+      <div className={classes.tabs}>
+        <h3
+          className={type=='all' ? classes.active : ""}
+          onClick={() => setType('all')}
+        >
+          All
+        </h3>
+        <h3
+          className={type==='purchase' ? classes.active : ""}
+          onClick={() => setType('purchase')}
+        >
+          Purchase
+        </h3>
+        <h3
+          className={type==='sale' ? classes.active : ""}
+          onClick={() => setType('sale')}
+        >
+          Sale
+        </h3>
+      </div>
+      <Assets/>
+    </div>
+  );
 }
