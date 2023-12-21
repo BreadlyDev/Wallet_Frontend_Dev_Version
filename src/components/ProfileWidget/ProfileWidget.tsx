e4ec82c413b5ee8d461ee858b01705af2fe1a5bc
AsanbaevBaitemir <baitemir1706@gmail.com>
2023-12-21 15:48:59 +0600

    profile widget

---
+++ src/components/ProfileWidget/ProfileWidget.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import classes from './ProfileWidget.module.scss'
+import Assets from './Assets';
+import History from './History/History';
+
+type Props = {};
+
+export default function ProfileWidget({}: Props) {
+  const [isAssets, setIsAssets] = useState(true)
+  return (
+    <div className={classes.ProfileWidget}>
+      <div className={classes.tabs}>
+        <h3
+          className={isAssets ? classes.active : ""}
+          onClick={() => setIsAssets(!isAssets)}
+        >
+          Assets
+        </h3>
+        <h3
+          className={!isAssets ? classes.active : ""}
+          onClick={() => setIsAssets(!isAssets)}
+        >
+          History
+        </h3>
+      </div>
+      <hr/>
+      {isAssets?
+      <Assets/>
+      :
+      <History/>
+      }
+    </div>
+  );
+}
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Profile.module.scss"
 import ProfileSection from "../../components/ProfileSection/ProfileSection";
+import ProfileWidget from "../../components/ProfileWidget/ProfileWidget";
 
 const Profile: React.FC = () => {
     return (
         <div className={styles.Profile}>
             <ProfileSection/>
+            <ProfileWidget/>
         </div>
     )
 }
