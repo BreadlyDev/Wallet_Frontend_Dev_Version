a50b57c1f9e585e24c839bddd17c860a56286e37
AsanbaevBaitemir <baitemir1706@gmail.com>
2023-12-23 22:43:14 +0600

    minor fixes

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,33 +1,50 @@
 import axios from "axios";
 import { api, api_min } from "../consts/api";
 
-export async function registerService(email:string, username:string, password:string) {
-  try{
-    const res = await axios.post(api + "/auth/register", {email, username, password})
-    localStorage.setItem('token', res.data.access_token)
+export async function registerService(
+  email: string,
+  username: string,
+  password: string
+) {
+  try {
+    const res = await axios.post(
+      api + "/auth/register",
+      { email, username, password },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    localStorage.setItem("token", res.data.access_token);
     console.log(res.data);
-  }
-  catch(e:any){
+  } catch (e: any) {
     console.log(e?.message);
   }
 }
-export async function loginService(username:string, password:string) {
-  try{
-    const res = await axios.post(api + "/auth/login", {username, password})
-    localStorage.setItem('token', res.data.access_token)
+export async function loginService(username: string, password: string) {
+  try {
+    const res = await axios.post(
+      api + "/auth/login",
+      { username, password },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    localStorage.setItem("token", res.data.access_token);
     console.log(res.data);
-  }
-  catch(e:any){
+  } catch (e: any) {
     console.log(e?.message);
   }
 }
 
 export async function googleService() {
-  try{
-    const res = await axios.get(api_min + "/auth/google/authorize") 
+  try {
+    const res = await axios.get(api_min + "/auth/google/authorize");
     console.log(res.data);
-  }
-  catch(e:any){
+  } catch (e: any) {
     console.log(e?.message);
   }
 }
--- src/services/BalanceService.ts
+++ src/services/BalanceService.ts
@@ -1,22 +1,22 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export async function asyncIncrementBalance(amount:number) {
-  // try{
-  //   const res = await axios.post("", {amount})
-  //   console.log(res);
-  // }
-  // catch(e:any){
-  //   console.log(e?.message);
+// export async function asyncIncrementBalance(amount:number) {
+//   // try{
+//   //   const res = await axios.post("", {amount})
+//   //   console.log(res);
+//   // }
+//   // catch(e:any){
+//   //   console.log(e?.message);
     
-  // }
-}
-export async function asyncDecrementBalance(amount:number) {
-  // try{
-  //   const res = await axios.post("", {amount})
-  //   console.log(res);
-  // }
-  // catch(e:any){
-  //   console.log(e?.message);
+//   // }
+// }
+// export async function asyncDecrementBalance(amount:number) {
+//   // try{
+//   //   const res = await axios.post("", {amount})
+//   //   console.log(res);
+//   // }
+//   // catch(e:any){
+//   //   console.log(e?.message);
     
-  // }
-}
+//   // }
+// }
